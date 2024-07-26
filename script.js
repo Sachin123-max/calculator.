4eae2170ac0fbe0c9401bb8297a4d6a1a06edac7
sachin <sachin1112245@gmail.com>
2024-07-26 12:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let inputBox = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button')
+let string = ''
+
+buttons.forEach(element =>{
+    element.addEventListener('click',(b) =>{
+        if(b.target.innerText == "="){
+            string = String(eval(string))
+            inputBox.value=string;
+        }
+        else if(b.target.innerText == 'AC'){
+            string =''
+            inputBox.value = string;
+        }
+        else if(b.target.innerText == 'C'){
+            string= string.substring(0,string.length-1)
+            inputBox.value = string;
+        }
+        else{
+            string += b.target.innerText
+            inputBox.value = string
+        }
+    })
+
+
+})
